4b685163775f171bd0fbcb444731fcd6fd28a5db
Nihal Srivastava <82692408+Nihal-Srivastava05@users.noreply.github.com>
2022-01-25 23:17:08 +0530

    Added Featured movies.

--- js/movies.js
+++ js/movies.js
@@ -1,5 +1,8 @@
 import { TMDB_API_KEY, BASE_URL } from "./apikey.js";
 
+const POPULAR_URL =
+  BASE_URL + "/discover/movie?sort_by=popularity.desc&" + TMDB_API_KEY;
+
 const ACTION_URL =
   BASE_URL +
   "/discover/movie?&with_genres=28&sort_by=popularity.desc&" +
@@ -10,12 +13,12 @@ const COMEDY_URL =
   "/discover/movie?&with_genres=35&sort_by=popularity.desc&" +
   TMDB_API_KEY;
 
-const DOCUMENTARY_URL = 
+const DRAMA_URL =
   BASE_URL +
-  "/discover/movie?&with_genres=99&sort_by=popularity.desc&" +
+  "/discover/movie?&with_genres=18&sort_by=popularity.desc&" +
   TMDB_API_KEY;
 
-const CRIME_URL = 
+const CRIME_URL =
   BASE_URL +
   "/discover/movie?&with_genres=80&sort_by=popularity.desc&" +
   TMDB_API_KEY;
@@ -23,14 +26,58 @@ const CRIME_URL =
 const IMG_URL = "https://image.tmdb.org/t/p/w500/";
 const action_list = document.getElementById("action-list");
 const comedy_list = document.getElementById("comedy-list");
-const documentry_list = document.getElementById("documentary-list");
+const drama_list = document.getElementById("drama-list");
 const crime_list = document.getElementById("crime-list");
+const featured_list = document.getElementById("featured-list");
 
 getMovies(ACTION_URL, action_list);
 getMovies(COMEDY_URL, comedy_list);
-getMovies(DOCUMENTARY_URL, documentry_list);
+getMovies(DRAMA_URL, drama_list);
 getMovies(CRIME_URL, crime_list);
 
+//Show Featured List
+showFeaturedMovies(ACTION_URL, featured_list);
+function showFeaturedMovies(url, list) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      list.innerHTML = "";
+      data.results.forEach((movie, i) => {
+        const { title, poster_path, vote_average, overview } = movie;
+        const movieEl = document.createElement("div");
+        if (i == 0) {
+          movieEl.classList.add("featured-slide");
+          movieEl.classList.add("active");
+        } else {
+          movieEl.classList.add("featured-slide");
+        }
+        movieEl.innerHTML = `
+        <img class="featured-img" src="${IMG_URL + poster_path}" alt="${title}">
+          <div class="featured-info">
+            <h2>${title}</h2>
+            <p>${overview}</p>
+          </div>
+        `;
+        list.appendChild(movieEl);
+      });
+      const navigationButtons = document.createElement("div");
+      navigationButtons.classList.add("featured-navigation");
+      data.results.forEach((movie, i) => {
+        const featuredBtn = document.createElement("div");
+        if (i == 0) {
+          featuredBtn.classList.add("featured-btn");
+          featuredBtn.classList.add("active");
+        } else {
+          featuredBtn.classList.add("featured-btn");
+        }
+        navigationButtons.appendChild(featuredBtn);
+      });
+      list.appendChild(navigationButtons);
+      scrollFeatured();
+    });
+}
+
+// Fetch movies form API
 function getMovies(url, list) {
   fetch(url)
     .then((res) => res.json())
@@ -39,6 +86,7 @@ function getMovies(url, list) {
     });
 }
 
+// Display movies on screen
 function showMovies(data, list) {
   list.innerHTML = "";
   data.forEach((movie) => {
@@ -63,6 +111,7 @@ function showMovies(data, list) {
   scrollMovies();
 }
 
+// Set Color in rating
 function getColor(vote) {
   if (vote >= 8) {
     return "green";
@@ -93,4 +142,59 @@ function scrollMovies() {
       }
     });
   });
+}
+
+// Featured Section slider
+function scrollFeatured() {
+  var slides = document.querySelectorAll(".featured-slide");
+  var btns = document.querySelectorAll(".featured-btn");
+  let currentSlide = 1;
+
+  var manualNav = function (manual) {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+
+      btns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+    });
+
+    slides[manual].classList.add("active");
+    btns[manual].classList.add("active");
+  };
+
+  btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+      manualNav(i);
+      currentSlide = i;
+    });
+  });
+
+  // JS for autoplay of featured
+  var repeat = function (activeClass) {
+    let active = document.getElementsByClassName("active");
+    let i = 1;
+
+    var repeater = () => {
+      setTimeout(function () {
+        [...active].forEach((activeSlide) => {
+          activeSlide.classList.remove("active");
+        });
+
+        slides[i].classList.add("active");
+        btns[i].classList.add("active");
+        i++;
+
+        if (slides.length == i) {
+          i = 0;
+        }
+        if (i >= slides.length) {
+          return;
+        }
+        repeater();
+      }, 10000);
+    };
+    repeater();
+  };
+  repeat();
 }
