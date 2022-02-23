adcdad3e73347fc0914ae36a8d21a08ce15f3426
VarunK1505 <kamathvarun15@gmail.com>
2022-02-23 23:50:43 +0530

    login registration with styles done

--- php/login.php
+++ php/login.php
@@ -5,60 +5,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Login</title>
-    <link rel="stylesheet" href="login.scss">
+    <link rel="stylesheet" href="log.css">
     
 </head>
 
 <body>
-    <link href="logincss.scss" rel="stylesheet/scss" type="text/css">
-    
-    <div class="container">
-    <div class="message signup">
-        <div class="btn-wrapper">
-        <button class="button" id="signup">Sign Up</button>
-        <button class="button" id="login"> Login</button>
-        </div>
-    </div>
-    <div class="form form--signup">
-        <div class="form--heading">Welcome! Sign Up</div>
-        <form autocomplete="off">
-        <input type="text" placeholder="Name">
-        <input type="email" placeholder="Email">
-        <input type="password" placeholder="Password">
-        <button class="button">Sign Up</button>
-        </form>
-    </div>
-    <div class="form form--login">
-        <div class="form--heading">Welcome back! </div>
-        <form autocomplete="off">
-        <input type="text" placeholder="Name">
-        <input type="password" placeholder="Password">
-        <button class="button">Login</button>
-        </form>
-    </div>
-    </div>
-    <!-- <div>
-        <form name="f1" action = "authentication.php" onsubmit = "return validation()" method = "POST">
-            <div class="container"></div>
-                <h1>Login</h1>
-                <p>Fill up the form with correct values</p>
-
-                <label for="email"><b>Email Address</b></label>
-                <input type="email" name="email"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" size="30" required>
-                <br>
-
-                <label for="password"><b>Password</b></label>
-                <input type="password" name="password" required>
-                <br>
-                
-                <input type="submit" name="save" value="Login"><br><br>
-            </div>      
+    <br><br>
+    <div class="main">
+        
+        <p class="sign" align="center">Login</p>
+        <form class="form1" name="f1" action="login.php" onsubmit = "return validation()" method = "post">
+        <input class="un" type="email" name="email"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" size="30" placeholder="Email" required>
+        <input class="pass" type="password" align="center" name="password" placeholder="Password">
+        <input class ="submit" type="submit" name="save" value="Login"><br><br>                         
         
         </form>
-    </div> -->
+    </div>
+ 
 </body>
 </html>
 
+<?php      
+    
+    include('config.php');  
+    $email = $_POST['email'];  
+    $password = $_POST['password'];  
+    
+    //to prevent from mysqli injection  
+    $email = stripcslashes($email);  
+    $password = stripcslashes($password);  
+    $email = mysqli_real_escape_string($con, $email);  
+    $password = mysqli_real_escape_string($con, $password);  
+    
+    $sql = "select *from users where email = '$email' and password = '$password'";  
+    $result = mysqli_query($con, $sql);  
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+    $count = mysqli_num_rows($result);  
+        
+    if($count == 1){  
+        echo "<h1><center> Login successful </center></h1>";  
+    }  
+    else{  
+        echo "<h1> Login failed. Invalid email $email or password.</h1>";  
+    }     
 
+?> 
 
 
